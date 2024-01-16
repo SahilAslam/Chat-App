939195ac0e49710b19185a7989f8ac4bafe29c65
SahilAslam <sahilaslam14@gmail.com>
2024-01-16 20:00:51 +0530

    added some components and a functionality to view profile

--- frontend/src/Components/Miscellaneous/SideDrawer.jsx
+++ frontend/src/Components/Miscellaneous/SideDrawer.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
+import { ChatState } from "../../Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
 
 const SideDrawer = () => {
-  return (
-    <div>SideDrawer</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
 
-export default SideDrawer
+  const { user } = ChatState();
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip hasArrow label="Search users" bg="gray.300" color="black">
+          <Button variant="ghost">
+            <IoSearch />
+            <Text display={{ base: "none", md: "flex" }} px="4">
+              Search
+            </Text>
+          </Button>
+        </Tooltip>
+
+        <Text fontSize="2xl" fontFamily="Lato">
+          GoChat
+        </Text>
+
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon />
+            </MenuButton>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.profilePic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default SideDrawer;
--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,18 +1,26 @@
-import { Box } from '@chakra-ui/react';
-import SideDrawer from '../Components/Miscellaneous/SideDrawer';
-import { ChatState } from '../Context/ChatProvider';
+import { Box } from "@chakra-ui/react";
+import SideDrawer from "../Components/Miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
+import MyChats from "../Components/MyChats";
+import ChatBox from "../Components/ChatBox";
 
 const ChatPage = () => {
   const { user } = ChatState();
   return (
     <div style={{ width: "100%" }}>
-      {user && <SideDrawer/>}
-      <Box>
-        {/* {user && <MyChats />}
-        {user && <ChatBox />} */}
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
       </Box>
     </div>
   );
-}
+};
 
-export default ChatPage
+export default ChatPage;
