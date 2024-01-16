4a9091f878bd0cdff94c39cdb00d9c57da6b1378
SahilAslam <sahilaslam14@gmail.com>
2024-01-16 11:43:22 +0530

    added controller for creating group chat

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -58,7 +58,7 @@ export const fetchChat = asyncHandler(async (req, res) => {
       .populate("groupAdmin", "-password")
       .populate("latestMessage")
       .sort({ updateAt: -1 });
-      
+
     if (chat) {
       chat = await User.populate(chat, {
         path: "latestMessage.sender",
@@ -68,6 +68,40 @@ export const fetchChat = asyncHandler(async (req, res) => {
     } else {
       throw new Error("coudn't find chat");
     }
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+export const createGroupChat = asyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please Fill all the feilds" });
+  }
+
+  let users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .json("More than 2 users are required to form a group chat");
+  }
+
+  users.push(req.user);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat?._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+      res.status(200).json(fullGroupChat)
   } catch (error) {
     res.status(400);
     throw new Error(error.message);
